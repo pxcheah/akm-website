3a7770ae8cdc94d380572c27d5be33f9c0d981b3
pxcheah <cpx188@gmail.com>
2021-01-22 03:15:25 +0800

    Lint fixes

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,7 @@ module.exports = {
   },
   rules: {
     'import/prefer-default-export': 'off',
+    'jsx-a11y/anchor-is-valid': 'warn',
     'react/jsx-props-no-spreading': 'off',
     'react/no-array-index-key': 'off',
     'react/prop-types': 'off',
--- src/components/product-categories/slide.tsx
+++ src/components/product-categories/slide.tsx
@@ -1,4 +1,4 @@
-import { Box, Center, Heading, Image } from '@chakra-ui/react';
+import { Box, Center, Heading } from '@chakra-ui/react';
 
 import { ProductCategory } from '@/types/home';
 
