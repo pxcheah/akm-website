8fa542d4bb1e28dc1129feca45089d04ebf86ffd
pxcheah <cpx188@gmail.com>
2021-01-24 23:43:41 +0800

    Removed tagline animation to improve web-vitals score

--- src/components/hero/tagline.tsx
+++ src/components/hero/tagline.tsx
@@ -1,22 +1,13 @@
-import { Container, Text, TextProps } from '@chakra-ui/react';
-import { motion } from 'framer-motion';
-
-const MotionText = motion.custom<Omit<TextProps, 'transition'>>(Text);
+import { Container, Text } from '@chakra-ui/react';
 
 const Tagline = () => (
   <Container maxW="80ch" px={8} color="white" fontWeight="bold" lineHeight={1.2}>
     <Text fontSize={{ base: 'xl', md: '3xl' }} align={{ md: 'center' }} opacity="0.85">
       Need industrial chemicals?
     </Text>
-    <MotionText
-      fontSize={{ base: '36px', md: '5xl' }}
-      align={{ md: 'center' }}
-      animate={{ opacity: 1, y: 0 }}
-      initial={{ opacity: 0, y: 20 }}
-      transition={{ ease: 'easeOut' }}
-    >
+    <Text fontSize={{ base: '36px', md: '5xl' }} align={{ md: 'center' }}>
       We&apos;ve got you covered!
-    </MotionText>
+    </Text>
   </Container>
 );
 
